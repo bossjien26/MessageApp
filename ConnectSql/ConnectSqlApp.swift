9082378432e8fbc860d569c7a148befa89fdeb84
jan <bossjien26@gmail.com>
2022-08-08 21:42:26 +0800

    Initial Commit

---
+++ ConnectSql/ConnectSqlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectSqlApp.swift
+//  ConnectSql
+//
+//  Created by 王復儀 on 2022/8/8.
+//
+
+import SwiftUI
+
+@main
+struct ConnectSqlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
